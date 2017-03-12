61cc812dd1ae30364d5de84f8ca615b884f4bbb5
shafeeq-rahman <shafeeqrahman96@gmail.com>
2017-03-12 17:03:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,32 @@ app.post('/create-user',function(req,res){
    });
 });
 
+app.post('/login', function(req,res){
+   var username = req.body.username;
+   var password = req.body.password;
+   var dbString = hash(password,salt);
+   pool.query('SELECT * from "user" WHERE username = $1', [username,dbString], function(err,result){
+       if(err) {
+           res.status(500).send(arr.toString());
+       }else {
+           if(result.rows.length === 0) {
+               res.send(403).send('username/password is invalid');
+           } else {
+               var dbString = result.rows[0].password;
+               var salt =dbString.split('$')[2];
+               var hashedPassword = hash(password,salt);
+               if(hashedPassword === dbString) {
+           res.send('User successfully created:' + username);
+               }
+               else {
+                   res.send('Invalid request');
+               }
+         }
+               
+     }
+   });
+});
+
 
 var pool = new Pool(config);
 app.get('/test-db', function (req,res){
