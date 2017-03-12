252ce081678930a162dae4723cba45d15a32090c
shafeeq-rahman <shafeeqrahman96@gmail.com>
2017-03-12 17:23:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
-console.log('Loaded!');
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+     if (request.readyState === XMLHttpRequest.DONE) {
+         if(request.status === 200) {
+             alert('Logged in successfully');
+         } else if (request.status === 403) {
+             alert('Username/Password is incorrect');
+         } else if (request.status === 500) {
+             alert('Something went wrong with the server');
+         }
+     }   
+    };
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://shafeeq-rahman.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+};
