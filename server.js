7365cc67ae05545fb556befc6edcf88898567b8a
shafeeq-rahman <shafeeqrahman96@gmail.com>
2017-03-12 17:36:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var crypto = require('Crypto');
+var crypto = require('crypto');
 var bodyParser = require('Body-parser');
 var config = {
     user: 'shafeeq-rahman',
